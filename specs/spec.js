80f95820260950c4435f872745f61597c6ee35cd
jozwright <jozwright@gmail.com>
2014-07-20 22:17:42 -0700

    Delete spec.js

--- specs/spec.js
+++
@@ -1,8 +0,0 @@
-describe("wiseBacronyms", function() {
-  it("displays the bacronym 'Accept differences' for the letter 'a'", function() {
-    wiseBacronyms("a").should.equal("Accept differences");
-  });
-  it("displays a bacronym for an entire word or phrase", function() {
-    wiseBacronyms("cab").should.equal("Count your blessings Accept differences Be kind");
-  });
-});
